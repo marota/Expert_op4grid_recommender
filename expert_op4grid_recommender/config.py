5833b75745626c3b18bc41699f2e8e045ddb05ae
marota <antoine-marot@live.fr>
2025-10-24 15:29:27 +0200

    making config file more generic to allow data - defaut - timestep parameters to be passed either in config file or in cli explicitly

--- expert_op4grid_recommender/config.py
+++ expert_op4grid_recommender/config.py
@@ -12,6 +12,15 @@
 from datetime import datetime
 import os
 
+from pathlib import Path # Import Path
+
+# --- Get Project Root Directory ---
+# Path to the directory containing this config.py file
+CONFIG_DIR = Path(__file__).parent.resolve()
+# Path to the project root directory (one level up from config.py's directory)
+PROJECT_ROOT = CONFIG_DIR.parent.resolve()
+# --- End Path Setup ---
+
 # -------------------
 #  Case Configuration
 # -------------------
@@ -21,21 +30,28 @@ import os
 # timestep = 32#1#47#18#13#22#9#1#15#1#35#10#14#14#13#22 #1 # 36
 # lines_defaut = "CHALOY631"#"CPVANL61ZMAGN"#"P.SAOL31RONCI"#"COUCHL31VOSNE"#"MAGNYY633"#"CPVANY633"#"CHALOL61CPVAN"#"C.REGL61ZMAGN"#"BEON L31CPVAN"#"CPVANL61ZMAGN"#"CPVANL31RIBAU"#"BEON L31CPVAN"#"MAGNYY633"#"BEON L31CPVAN"##AISERL31RONCI, P.SAOL31RONCI, AISERL31MAGNY, BEON L31CPVAN, "FRON5L31LOUHA"
 
-DATE = datetime(2024, 8, 29)#datetime(2024, 9, 19)#datetime(2024, 8, 28)#datetime(2024, 11, 27)#datetime(2024, 12, 9)#datetime(2024, 12, 7)#datetime(2024, 12, 7)#datetime(2024, 9, 19)#datetime(2024, 11, 27)#datetime(2024, 9, 19)#datetime(2024, 11, 27)#datetime(2024, 9, 19)#datetime(2024, 11, 25)#datetime(2024, 11, 25)#datetime(2024, 11, 25)#datetime(2024, 12, 9)#datetime(2024, 12, 2)#datetime(2024, 8, 28)  # we choose a date for the chronic
-TIMESTEP = 1#32#1#47#18#13#22#9#1#15#1#35#10#14#14#13#22 #1 # 36
-LINES_DEFAUT = ["CPVANL61ZMAGN"]#"CHALOY631"#"CPVANL61ZMAGN"#"P.SAOL31RONCI"#"COUCHL31VOSNE"#"MAGNYY633"#"CPVANY633"#"CHALOL61CPVAN"#"C.REGL61ZMAGN"#"BEON L31CPVAN"#"CPVANL61ZMAGN"#"CPVANL31RIBAU"#"BEON L31CPVAN"#"MAGNYY633"#"BEON L31CPVAN"##AISERL31RONCI, P.SAOL31RONCI, AISERL31MAGNY, BEON L31CPVAN, "FRON5L31LOUHA"
+DATE = datetime(2024, 12, 7)#datetime(2024, 12, 7)#datetime(2024, 9, 19)#datetime(2024, 11, 27)#datetime(2024, 9, 19)#datetime(2024, 11, 27)#datetime(2024, 9, 19)#datetime(2024, 11, 25)#datetime(2024, 11, 25)#datetime(2024, 11, 25)#datetime(2024, 12, 9)#datetime(2024, 12, 2)#datetime(2024, 8, 28)  # we choose a date for the chronic
+TIMESTEP = 9#9#1#15#1#35#10#14#14#13#22 #1 # 36
+LINES_DEFAUT = ["CHALOL61CPVAN"]#"CHALOL61CPVAN"#"C.REGL61ZMAGN"#"BEON L31CPVAN"#"CPVANL61ZMAGN"#"CPVANL31RIBAU"#"BEON L31CPVAN"#"MAGNYY633"#"BEON L31CPVAN"##AISERL31RONCI, P.SAOL31RONCI, AISERL31MAGNY, BEON L31CPVAN, "FRON5L31LOUHA"
 CASE_NAME = "defaut_" + "_".join(map(str, LINES_DEFAUT)) + "_t" + str(TIMESTEP)
 
 # -------------------
-#  Environment & Pathsdatetime(2024, 9, 19)#
+#  Environment & Paths --- Use PROJECT_ROOT to build paths ---
 # -------------------
-ENV_FOLDER = "../data"
+# ENV_FOLDER = "../data" # OLD - Incorrect when run from root
+ENV_FOLDER = PROJECT_ROOT / "data" # NEW - Path relative to project root
 ENV_NAME = "env_dijon_v2_assistant"
-ENV_PATH = os.path.join(ENV_FOLDER, ENV_NAME)
-ACTION_SPACE_FOLDER = os.path.join(ENV_FOLDER,"action_space")
-FILE_ACTION_SPACE_DESC = "reduced_model_actions.json"
-ACTION_FILE_PATH = os.path.join(ACTION_SPACE_FOLDER, FILE_ACTION_SPACE_DESC)
-SAVE_FOLDER_VISUALIZATION = "../Overflow_Graph" # Directory to save graph visualizations
+ENV_PATH = ENV_FOLDER / ENV_NAME # Use Path object joining
+
+# ACTION_SPACE_FOLDER = os.path.join(ENV_FOLDER,"action_space") # OLD
+ACTION_SPACE_FOLDER = ENV_FOLDER / "action_space" # NEW
+
+FILE_ACTION_SPACE_DESC = "reduced_model_actions.json" #"actions_repas_most_frequent_topologies_revised.json"
+# ACTION_FILE_PATH = os.path.join(ACTION_SPACE_FOLDER, FILE_ACTION_SPACE_DESC) # OLD
+ACTION_FILE_PATH = ACTION_SPACE_FOLDER / FILE_ACTION_SPACE_DESC # NEW
+
+# SAVE_FOLDER_VISUALIZATION = "../Overflow_Graph" # OLD
+SAVE_FOLDER_VISUALIZATION = PROJECT_ROOT / "Overflow_Graph" # NEW - Path relative to project root
 
 # -------------------
 #  User Parameters
